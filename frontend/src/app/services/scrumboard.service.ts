db88ea93026520a73dbb119bbb1031842b38f259
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-11-29 10:00:41 +0100

    Update scrumboard.service.ts

--- frontend/src/app/services/scrumboard.service.ts
+++ frontend/src/app/services/scrumboard.service.ts
@@ -13,7 +13,7 @@ export class ScrumboardService{
 
     const headers = new HttpHeaders({
 
-      'Content-Type': 'application/json',
+      'Accept': 'application/json',
 
       'Authorization': `Bearer ${auth_token}`
 
